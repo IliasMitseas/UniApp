bd8710464ba5e4da9de74a828f47a13bcfbd3e4f
ilias.mitseas <ilias.mitseas@gmail.com>
2025-02-02 20:17:21 +0200

    Arxiko GUI

---
+++ src/main/java/ilias/uniapp/UniApp.java
@@ -0,0 +1,11 @@
+package ilias.uniapp;
+
+import ilias.uniapp.Forms.MainForm;
+
+public class UniApp {
+
+    public static void main(String[] args) {
+        MainForm.showMainForm();
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ilias/uniapp/db/University.java
@@ -0,0 +1,102 @@
+package ilias.uniapp.db;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class University {
+
+    @Id
+    private String id;
+    private String name;
+    private String domain;
+    private String webPage;
+    private String alphaTwoCode;
+    private String country;
+    private String stateProvince;
+
+
+    public University(String id, String name, String domain, String webPage, String alphaTwoCode, String country, String stateProvince) {
+        this.id = id;
+        this.name = name;
+        this.domain = domain;
+        this.webPage = webPage;
+        this.alphaTwoCode = alphaTwoCode;
+        this.country = country;
+        this.stateProvince = stateProvince;
+    }
+
+    public University() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getWebPage() {
+        return webPage;
+    }
+
+    public void setWebPage(String webPage) {
+        this.webPage = webPage;
+    }
+
+    public String getAlphaTwoCode() {
+        return alphaTwoCode;
+    }
+
+    public void setAlphaTwoCode(String alphaTwoCode) {
+        this.alphaTwoCode = alphaTwoCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStateProvince() {
+        return stateProvince;
+    }
+
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", domain='" + domain + '\'' +
+                ", webPage='" + webPage + '\'' +
+                ", alphaTwoCode='" + alphaTwoCode + '\'' +
+                ", country='" + country + '\'' +
+                ", stateProvince='" + stateProvince + '\'' +
+                '}';
+    }
+}
